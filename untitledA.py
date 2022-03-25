fdc7e6045bd137d211203b054d70726b08fa602d
rana71099 <100765199+rana71099@users.noreply.github.com>
2022-03-25 19:32:39 +0300

    H.WORK3

---
+++ untitledA.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 22 09:13:51 2022
+
+@author: Pc
+"""
+
+read_line = 'ex3_text (1).txt'
+with open(read_line) as fp:
+    words=sentence.split()
+    if (isinstance(reverse_word, str)==False):
+        return("invalid input")
+    try:
+        f=open(file)
+    except FileNotFoundError:
+        return("file not found")
+    else:
+        lines=fp.readlines()
+        if( len(lines) < (n-1)):
+            return("line "+ str(n) +" doesn't exist")
+        return(lines[n-1])
+    val='The word was not found'
+    line = fp.readline()
+    cnt = 1
+
+        line = fp.readline()
+  
